3a8a11e21035ef195d255a697b685a6ade854083
Andrej Mazur <andrej.mazur@outlook.com>
2021-08-14 20:54:21 +0300

    fix

--- app/src/main/java/com/example/pomodoro/CountdownTimerAdapter.kt
+++ app/src/main/java/com/example/pomodoro/CountdownTimerAdapter.kt
@@ -2,13 +2,14 @@ package com.example.pomodoro
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncDifferConfig
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.example.pomodoro.databinding.TimerItemBinding
 
 class CountdownTimerAdapter(
     private val listener: CountdownTimerListener
-) : ListAdapter<CountdownTimer, CountdownTimerViewHolder>(itemComparator) {
+) : ListAdapter<CountdownTimer, CountdownTimerViewHolder>(AsyncDifferConfig.Builder(CountdownTimerDiffCallback()).build()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountdownTimerViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -19,22 +20,19 @@ class CountdownTimerAdapter(
     override fun onBindViewHolder(holderCountdown: CountdownTimerViewHolder, position: Int) {
         holderCountdown.bind(getItem(position))
     }
+}
 
-    private companion object {
+private class CountdownTimerDiffCallback : DiffUtil.ItemCallback<CountdownTimer>() {
 
-        private val itemComparator = object : DiffUtil.ItemCallback<CountdownTimer>() {
-
-            override fun areItemsTheSame(oldItem: CountdownTimer, newItem: CountdownTimer): Boolean {
-                return oldItem.id == newItem.id
-            }
-
-            override fun areContentsTheSame(oldItem: CountdownTimer, newItem: CountdownTimer): Boolean {
-                return oldItem.remainingTime.millisToSeconds() == newItem.remainingTime.millisToSeconds()
-                        && oldItem.isStarted == newItem.isStarted
-                        && oldItem.isFinished == newItem.isFinished
-            }
-
-            override fun getChangePayload(oldItem: CountdownTimer, newItem: CountdownTimer) = Any()
-        }
+    override fun areItemsTheSame(oldItem: CountdownTimer, newItem: CountdownTimer): Boolean {
+        return oldItem.id == newItem.id
     }
+
+    override fun areContentsTheSame(oldItem: CountdownTimer, newItem: CountdownTimer): Boolean {
+        return oldItem.remainingTime.millisToSeconds() == newItem.remainingTime.millisToSeconds()
+                && oldItem.isStarted == newItem.isStarted
+                && oldItem.isFinished == newItem.isFinished
+    }
+
+    override fun getChangePayload(oldItem: CountdownTimer, newItem: CountdownTimer) = Any()
 }
